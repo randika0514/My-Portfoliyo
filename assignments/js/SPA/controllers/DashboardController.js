faa76f96ac5c0f3ac51657d505594e6fe2c10a22
randika0514 <randikadimantha@gmail.com>
2022-10-12 23:11:54 +0530

    Content Updated

--- assignments/js/SPA/controllers/DashboardController.js
+++ assignments/js/SPA/controllers/DashboardController.js
@@ -1,9 +1,17 @@
 $("#dashboard").click(function (){
   $("#dashboardContent").css('display','block');
   $("#customerContent").css('display','none');
+  $("#ItemContent").css('display','none');
 });
 
 $("#customer").click(function (){
     $("#dashboardContent").css('display','none');
     $("#customerContent").css('display','block');
+    $("#ItemContent").css('display','none');
+});
+
+$("#item").click(function (){
+    $("#ItemContent").css('display','block');
+    $("#dashboardContent").css('display','none');
+    $("#customerContent").css('display','none');
 });
